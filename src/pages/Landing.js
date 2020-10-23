3712c7b2156b12a86429b4129a418a70a5458ac7
xsadia <lipearthur@hotmail.com>
2020-10-23 16:23:37 -0300

    First commit :monkey:

---
+++ src/pages/Landing.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/pages/Landing.css';
+
+function Landing(){
+    return(
+        <div id='landing-page'>
+            <div className='content-wrapper'>
+               <main>
+                   <h1>Astronomy Picture Of the Day</h1>
+               </main>
+            </div>
+        </div>
+    )
+}
+
+export default Landing;
